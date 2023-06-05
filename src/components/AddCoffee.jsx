37d36ceafed3bd959c5816fa1f41b3de4afaa456
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-06-05 07:25:58 +0600

    68.3

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,5 +1,37 @@
 const AddCoffee = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>add a coffee</h2>
+      <form>
+        <div className="flex">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Your Coffee Name"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Available Quantity"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
