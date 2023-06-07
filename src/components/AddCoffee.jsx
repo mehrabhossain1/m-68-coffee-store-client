22bf3b39874734c60f923ef5fd478198390726fb
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-06-07 22:30:42 +0600

    server a paise data

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -21,6 +21,19 @@ const AddCoffee = () => {
       photo,
     };
     console.log(newCoffee);
+
+    // send data to the server
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
