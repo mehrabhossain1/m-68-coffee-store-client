8f078d0fdff7acdf5014c844cebb80888ab1c81e
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-06-12 00:10:05 +0600

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import "./App.css";
+import CoffeeCard from "./components/CoffeeCard";
 
 function App() {
   const coffees = useLoaderData();
@@ -9,6 +10,9 @@ function App() {
       <h1 className="text-6xl text-purple-600">
         Hot Hot Cold Coffee: {coffees.length}
       </h1>
+      {coffees.map((coffee) => (
+        <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+      ))}
     </>
   );
 }
---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,19 @@
+const CoffeeCard = ({ coffee }) => {
+  console.log(coffee);
+  return (
+    <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src={coffee.photo} alt="Movie" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
