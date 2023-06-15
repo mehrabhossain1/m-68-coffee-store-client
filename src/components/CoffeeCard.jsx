a4d19b68f819df64c1bdee40918d676b3b751bdc
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-06-15 22:43:34 +0600

    swal confirm

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ coffee }) => {
   // console.log(coffee);
 
@@ -6,6 +8,19 @@ const CoffeeCard = ({ coffee }) => {
 
   const handleDelete = (_id) => {
     console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
   };
 
   return (
